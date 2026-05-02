a9ca04a9f464b2e4bafe036e7acd798a206520c7
gala mohammed-1525 <galaa.2023@hotmail.com>
2026-05-03 01:30:00 +0300

    implement Task 3 using Binary Semaphore to regulate CPU access

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -127,6 +127,12 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
+        try {
+        // Task 3: Acquire semaphore before entering Critical Section (CPU execution)
+        SharedResources.cpuSemaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            }
         
         try {
             if (startTime == -1) {
@@ -190,6 +196,7 @@ class Process implements Runnable {
         } finally {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+            SharedResources.cpuSemaphore.release();
         }
     }
     
